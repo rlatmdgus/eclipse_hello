29e329c1fb2dbf71ae47eda10c717f7ebf13bdac
KimSeunghyun <gha0810@naver.com>
2019-10-31 09:34:30 +0900

    refs/remotes/origin/master line delete

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -2,10 +2,7 @@
 public class PushPrint {
 	public void Print() {
 		System.out.println("Print Test");
-<<<<<<< HEAD
 		System.out.println("Eclipse Print Test");
-=======
 		System.out.println("GitHub Print Test");
->>>>>>> refs/remotes/origin/master
 	}
 }
